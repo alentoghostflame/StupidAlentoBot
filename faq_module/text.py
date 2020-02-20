ac78ec75548fbfb4dce87ebbc4187f53cc01f9d7
BuildTools <unconfigured@null.spigotmc.org>
2020-02-20 12:59:57 -0600

    Added a few more lines

--- faq_module/text.py
+++ faq_module/text.py
@@ -1,10 +1,16 @@
 FAQ_ADMIN_HELP = "This is the central admin command for managing the faq feature. Valid first arguments: " \
                  "`add_keyword`, `remove_keyword`, `list_keywords`, `add_edit_role`, `remove_edit_role`, " \
                  "`list_edit_roles`."
+LACK_PERMISSION_OR_ROLE = "You don't have the role or permission to use that command."
+LACK_ADMINISTRATOR = "You lack the Administrator permission."
 TOO_MANY_ARGUMENTS = "Too many arguments specified. Did you forget to wrap multi-word arguments in quotes?"
 INVALID_FIRST_ARGUMENT = "Invalid first argument."
+INVALID_ROLE = "Role specified not found."
 DONT_NEED_ADD_ARGS = "...you don't need additional arguments for this."
 DONT_NEED_ARG2 = "...you don't need another argument for this command."
+ROLE_ADDED = "Role added to list."
+ROLE_REMOVED = "Role removed from list."
+ROLE_NOT_FOUND = "Specified role not found in list."
 ADD_KEYWORD_HELP = "Adds a keyword to the FAQ system. Specify a keyword and a statement that the keyword should give."
 ADD_KEYWORD_MISSING_STATEMENT = "Statement appears to be missing."
 ADD_KEYWORD_OVERWROTE = "Overwrote keyword `{}`"
@@ -12,3 +18,8 @@ ADD_KEYWORD_NEW = "Created keyword `{}`"
 REMOVE_KEYWORD_HELP = "Removes a keyword from the FAQ system. Specify a keyword to be removed."
 REMOVE_KEYWORD_FOUND = "Deleted keyword `{}`"
 REMOVE_KEYWORD_MISSING = "Couldn't find keyword `{}`"
+ADD_EDIT_ROLE_HELP = "Allows users with the given role to create, edit, and remove keywords and/or statements. " \
+                     "@mention the role or provide the role id after this command."
+REMOVE_EDIT_ROLE_HELP = "Removes a role from the list of roles that can create, edit, and remove keywords and/or " \
+                        "statements. @mention the role or provide the role id after this command."
+
