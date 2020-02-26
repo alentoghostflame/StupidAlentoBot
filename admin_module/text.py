90755c33a4db656d90d4f0d67258836f95fed915
BuildTools <unconfigured@null.spigotmc.org>
2020-02-25 22:01:52 -0600

    Added, moved things

--- admin_module/text.py
+++ admin_module/text.py
@@ -1,6 +1,16 @@
+ADMIN_HELP = "This command "
+
+
+WARN_HELP = "This command warns the given user for 30 minutes. Use `;warn @user` replacing `@user` with mentioning " \
+            "the user."
+WARN_DEFAULT_REASON = "No reason given."
 WARN_REASON = "{} Warned because: {}"
-DEFAULT_WARN_REASON = "No reason given."
 WARN_GIVEN = "5 minute warn given."
 WARN_DOUBLE = "Already had a warn, muted."
-WARN_MUTE_MISSING = "Can't do 2 warns -> mute due to mute not being assigned a role."
 WARN_DOUBLE_REASON = "{} Doubled warned because: {}"
+
+WARN_MISSING_ROLE = "You lack the role or permission required to warn users."
+WARN_CANT_WARN_WARNERS = "You can't warn the warners"
+WARN_ROLE_MISSING = "The selected warn role is non-existent. Fix that by using `;warn_admin set_warn_role @role` " \
+                    "replacing @role with mentioning the role."
+WARN_MUTE_MISSING = "Can't do 2 warns -> mute due to mute not being assigned a role."
