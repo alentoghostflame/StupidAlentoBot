03c396481d5e219bd624b83af94de6a11f6225dd
BuildTools <unconfigured@null.spigotmc.org>
2020-02-25 22:15:04 -0600

    Commented out call to update_data(), other comment changes

--- start.py
+++ start.py
@@ -1,7 +1,6 @@
-from datetime import datetime
 from main_bot import StupidAlentoBot
-import stupid_utils
-# import traceback
+from universal_module import utils
+# from datetime import datetime
 import logging
 import sys
 import os
@@ -11,21 +10,12 @@ FORMAT = "[{asctime}][{filename}][{lineno:3}][{funcName}][{levelname}] {message}
 LOGGING_LEVEL = logging.DEBUG
 
 
-# def log_exception_handler(error_type, value, tb):
-#     # TODO: Unify logging errors.
-#     the_logger = logging.getLogger("Main")
-#     the_logger.critical("Uncaught exception:\n"
-#                         "Type: {}\n"
-#                         "Value: {}\n"
-#                         "Traceback:\n {}".format(str(error_type), str(value), "".join(traceback.format_tb(tb))))
-
-
 def setup_logging():
     setup_logger = logging.getLogger("Main")
     log_format = logging.Formatter(FORMAT, style="{")
 
     os.makedirs("logs", exist_ok=True)
-    time_string = datetime.now().isoformat()
+    # time_string = datetime.now().isoformat()
     # log_file_handler = logging.FileHandler("logs/SAB {}.log".format(time_string), mode="w+")
     log_latest_handler = logging.FileHandler("logs/SAB Latest.log", mode="w+")
 
@@ -39,7 +29,7 @@ def setup_logging():
     setup_logger.addHandler(log_console_handler)
 
     setup_logger.setLevel(LOGGING_LEVEL)
-    sys.excepthook = stupid_utils.log_exception_handler
+    sys.excepthook = utils.log_exception_handler
 
 
 stupid_bot = StupidAlentoBot()
@@ -49,7 +39,7 @@ try:
     # logger = logging.getLogger("Main")
     # stupid_bot = StupidAlentoBot()
     stupid_bot.load_data()
-    stupid_bot.update_data()
+    # stupid_bot.update_data()
     stupid_bot.run()
     stupid_bot.save_data()
 except:
@@ -59,6 +49,6 @@ except:
 # # logger = logging.getLogger("Main")
 # # stupid_bot = StupidAlentoBot()
 # stupid_bot.load_data()
-# stupid_bot.update_data()
+# # stupid_bot.update_data()
 # stupid_bot.run()
 # stupid_bot.save_data()
