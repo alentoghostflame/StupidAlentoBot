5a129e821863ed7b02630c7b16eda1bf54f78ce3
Alex Schoenhofen <alexanderschoenhofen@gmail.com>
2020-03-12 18:28:52 -0500

    Removed some useless bits

--- steam_announcement_module/steam_announcement.py
+++ steam_announcement_module/steam_announcement.py
@@ -2,10 +2,7 @@ from steam_announcement_module.background_task import announcement_checker
 from steam_announcement_module.admin import steam_announcement_admin
 from storage_module.disk_storage import DiskStorage
 from discord.ext import commands, tasks
-import universal_module.utils
-# import discord
 import logging
-import sys
 
 
 logger = logging.getLogger("Main")
@@ -37,41 +34,3 @@ class SteamAnnouncementCog(commands.Cog, name="Steam Announcement Module"):
     @tasks.loop(minutes=10)
     async def announcement_checker_loop(self):
         await announcement_checker(self.bot, self.disk_storage)
-
-
-# import discord, urllib.request, json, datetime
-#
-# def meta_reader(given_text):
-#     split_text = given_text[1:-1].split(" ")
-#     output = dict()
-#     for bit in split_text:
-#         if "name=" in bit[:5]:
-#             output["name"] = bit[5:].strip("\"")
-#         elif "property=" in bit[:9]:
-#             output["property"] = bit[9:].strip("\"")
-#         elif "content=" in bit[:8]:
-#             output["content"] = bit[8:].strip("\"")
-#     return output
-#
-# url = urllib.request.urlopen("http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=1178460&count=3&maxlength=200&format=json")
-# full_data = json.loads(url.read().decode())
-# data = full_data["appnews"]["newsitems"][0]
-#
-# announcement_url = urllib.request.urlopen(data["url"])
-# announcement_data = announcement_url.read().decode()
-# found_image = None
-# for stuff in announcement_data.split("\n"):
-#     text = stuff.strip()
-#     if "meta" in text:
-# If Meta and property="og:image"
-#         found_image = meta_reader(text).get("content", None)
-#
-# embed = discord.Embed(title=data["title"], color=0xffff00)
-#
-# embed.add_field(name="Preview", value=data["contents"], inline=False)
-# embed.add_field(name="URL", value=announcement_url.url, inline=False)
-# embed.set_image(url=found_image)
-# embed.timestamp = datetime.datetime.utcfromtimestamp(data["date"])
-#
-# await context.send(embed=embed)
-
