65c11954ec4750c07030e6229eda09c8fb13379b
Alex Schoenhofen <alexanderschoenhofen@gmail.com>
2020-03-13 21:03:50 -0500

    Added being able to give an image to the embed

--- faq_module/provide_faq.py
+++ faq_module/provide_faq.py
@@ -1,4 +1,4 @@
-from universal_module import utils
+import universal_module.utils
 import logging
 import discord
 import typing
@@ -7,7 +7,7 @@ import re
 
 
 logger = logging.getLogger("Main")
-sys.excepthook = utils.log_exception_handler
+sys.excepthook = universal_module.utils.log_exception_handler
 
 
 async def provide_info(faq_phrases: typing.Dict[str, str], message: discord.Message):
@@ -16,7 +16,7 @@ async def provide_info(faq_phrases: typing.Dict[str, str], message: discord.Mess
 
     provide_info_recursive(faq_phrases, message.content, embed, found_keys)
 
-    if embed.fields:
+    if embed.fields or embed.image:
         await message.channel.send(embed=embed)
 
 
@@ -25,7 +25,12 @@ def provide_info_recursive(faq_phrases: typing.Dict[str, str], message_content, 
     keywords = comp.findall(message_content)
     for keyword in keywords:
         if keyword in faq_phrases and keyword not in found_keys:
-            found_keys.add(keyword)
-            embed.add_field(name=keyword, value=faq_phrases[keyword], inline=False)
-            logger.debug("Found keyword {} with value {}".format(keyword, faq_phrases[keyword]))
-            provide_info_recursive(faq_phrases, faq_phrases[keyword], embed, found_keys)
+            if universal_module.utils.is_image_url(faq_phrases[keyword]):
+                found_keys.add(keyword)
+                embed.set_image(url=faq_phrases[keyword])
+                logger.debug("Found image keyword {} with value {}".format(keyword, faq_phrases[keyword]))
+            else:
+                found_keys.add(keyword)
+                embed.add_field(name=keyword, value=faq_phrases[keyword], inline=False)
+                logger.debug("Found keyword {} with value {}".format(keyword, faq_phrases[keyword]))
+                provide_info_recursive(faq_phrases, faq_phrases[keyword], embed, found_keys)
