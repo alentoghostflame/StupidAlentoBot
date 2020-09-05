7837c7069ec203fb153e21b02069886c028c1b6d
Alex Schoenhofen <alexanderschoenhofen@gmail.com>
2020-09-05 18:24:12 -0500

    Removed commented code
    Signed-off-by: Alex Schoenhofen <alexanderschoenhofen@gmail.com>

--- moderation_module/moderation.py
+++ moderation_module/moderation.py
@@ -8,24 +8,6 @@ import logging
 logger = logging.getLogger("main_bot")
 
 
-# class ModeratorModule:
-#     def __init__(self, discord_bot: DiscordBot):
-#         self.discord_bot: DiscordBot = discord_bot
-#         self.punish_manager: PunishmentManager = PunishmentManager(self.discord_bot.storage)
-#
-#     def register_cogs(self):
-#         logger.info("Registering cogs for Moderation.")
-#         self.discord_bot.add_cog(PunishmentCog(self.discord_bot.storage, self.punish_manager, self.discord_bot.bot))
-#         self.discord_bot.add_cog(WordBanCog(self.discord_bot.storage, self.punish_manager, self.discord_bot.bot))
-#         self.discord_bot.add_cog(GuildLoggingCog(self.discord_bot.storage))
-#
-#     def load(self):
-#         pass
-#
-#     def save(self):
-#         pass
-
-
 class ModeratorModule(BaseModule):
     def __init__(self, bot, storage):
         BaseModule.__init__(self, bot, storage)
