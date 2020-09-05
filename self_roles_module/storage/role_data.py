91cfb1ae0e9741645ba7b48a79dd16546a0e3ca1
Alex Schoenhofen <alexanderschoenhofen@gmail.com>
2020-09-05 16:25:08 -0500

    Now properly migrated to be compatible
    Signed-off-by: Alex Schoenhofen <alexanderschoenhofen@gmail.com>

--- self_roles_module/storage/role_data.py
+++ self_roles_module/storage/role_data.py
@@ -4,6 +4,5 @@ import typing
 
 @guild_data_transformer(name="self_roles_data")
 class RoleSelfAssignData:
-    def __init__(self, config, guild_id):
-        super().__init__(config, guild_id)
+    def __init__(self):
         self.roles: typing.Dict[str, int] = dict()
