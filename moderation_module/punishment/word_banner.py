e726dd1a7b1462dc980fe44ea1b8a4b4bc8648d1
Alex Schoenhofen <alexanderschoenhofen@gmail.com>
2020-09-05 00:10:00 -0500

    Removed unneeded comment
    Signed-off-by: Alex Schoenhofen <alexanderschoenhofen@gmail.com>

--- moderation_module/punishment/word_banner.py
+++ moderation_module/punishment/word_banner.py
@@ -30,7 +30,6 @@ class WordBanCog(commands.Cog, name="Word Ban"):
                 split_message = message.content.split()
                 for possible_word in split_message:
                     stripped_word = possible_word.strip("_`*~|\\").lower()
-                    # if possible_word.lower() in ban_config.banned_words:
                     if stripped_word in ban_config.banned_words:
                         punish_config = self.storage.guilds.get(message.guild.id, "punishment_config")
                         await delete_message_and_warn(self.punish_manager, punish_config, message, possible_word)
