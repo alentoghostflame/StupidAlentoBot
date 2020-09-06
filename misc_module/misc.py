08cd70958ff8b48bdafa2d44ffc92c76484a8b9c
Alex Schoenhofen <alexanderschoenhofen@gmail.com>
2020-09-05 21:36:02 -0500

    Adapted the original misc module for the current setup
    Signed-off-by: Alex Schoenhofen <alexanderschoenhofen@gmail.com>

--- misc_module/misc.py
+++ misc_module/misc.py
@@ -2,29 +2,22 @@ from misc_module.storage import WelcomeConfig, SteamAnnouncementConfig, SteamAnn
 from misc_module.steam_announcements import SteamAnnouncementCog
 from misc_module.super_jank_ville import SidebarStatusCog
 from misc_module.welcomes import WelcomeCog
-from alento_bot import DiscordBot
-from discord.ext import commands
+from alento_bot import BaseModule
 import logging
-import discord
 
 
 logger = logging.getLogger("main_bot")
 
 
-class MiscModule:
-    def __init__(self, discord_bot: DiscordBot):
-        self.discord_bot: DiscordBot = discord_bot
-        # self.discord_bot.storage.guilds.register_data_name("faq_config", FAQConfig)
-        # self.faq_manager: FAQManager = FAQManager(self.discord_bot.storage)
+class MiscModule(BaseModule):
+    def __init__(self, bot, storage):
+        BaseModule.__init__(self, bot, storage)
         # noinspection PyArgumentList
-        self.steam_cache = SteamAnnouncementCache(self.discord_bot.storage.config)
-        self.discord_bot.storage.caches.register_cache(self.steam_cache, "steam_announcement_cache")
-        self.discord_bot.storage.guilds.register_data_name("welcome_config", WelcomeConfig)
-        self.discord_bot.storage.guilds.register_data_name("steam_announcement_config", SteamAnnouncementConfig)
+        self.steam_cache = SteamAnnouncementCache(self.storage.config)
+        self.storage.caches.register_cache(self.steam_cache, "steam_announcement_cache")
+        self.storage.guilds.register_data_name("welcome_config", WelcomeConfig)
+        self.storage.guilds.register_data_name("steam_announcement_config", SteamAnnouncementConfig)
 
-    def register_cogs(self):
-        logger.info("Registering cogs for Misc.")
-        # self.discord_bot.add_cog(FAQCog(self.discord_bot.storage, self.faq_manager, self.discord_bot.bot))
-        self.discord_bot.add_cog(WelcomeCog(self.discord_bot.storage))
-        self.discord_bot.add_cog(SteamAnnouncementCog(self.discord_bot.bot, self.discord_bot.storage, self.steam_cache))
-        self.discord_bot.add_cog(SidebarStatusCog(self.discord_bot.bot))
+        self.add_cog(WelcomeCog(self.storage))
+        self.add_cog(SteamAnnouncementCog(self.bot, self.storage, self.steam_cache))
+        self.add_cog(SidebarStatusCog(self.bot))
--- start.py
+++ start.py
@@ -1,10 +1,12 @@
 from alento_bot import StupidAlentoBot, CoreModule
 # from moderation_module import ModeratorModule
+# from eve_module import EVEModule
 # from faq_module import FAQModule
 # from misc_module import MiscModule
-# from self_roles_module import SelfRoleModule
 # from misc2_module import MiscModule as Misc2Module
-# from eve_module import EVEModule
+# from self_roles_module import SelfRoleModule
+
+
 import logging
 
 
@@ -16,10 +18,11 @@ discord_bot = StupidAlentoBot()
 try:
     discord_bot.add_module(CoreModule)
     # discord_bot.add_module(ModeratorModule)
-    # discord_bot.add_module(FAQModule)
-    # discord_bot.add_module(SelfRoleModule)
-    # discord_bot.add_module(Misc2Module)
     # discord_bot.add_module(EVEModule)
+    # discord_bot.add_module(FAQModule)
+    # discord_bot.add_module(MiscModule)
+    # discord_bot.add_module(Misc2Module)
+    # discord_bot.add_module(SelfRoleModule)
 
     discord_bot.init_modules()
     discord_bot.init_cogs()
