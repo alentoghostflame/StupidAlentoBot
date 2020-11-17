bf74c749b19b05129a4c0ac4705eec2ed9286b22
Alex Schoenhofen <alexanderschoenhofen@gmail.com>
2020-11-16 19:32:46 -0600

    Now uses better random function
    Signed-off-by: Alex Schoenhofen <alexanderschoenhofen@gmail.com>

--- misc_module/welcomes/commands/welcome_on_join.py
+++ misc_module/welcomes/commands/welcome_on_join.py
@@ -1,12 +1,7 @@
 from misc_module.storage import WelcomeConfig
-from misc_module.welcomes.commands import text
-from discord.ext import commands
-import misc_module.text
 import logging
 import discord
 import random
-import typing
-import re
 
 
 logger = logging.getLogger("main_bot")
@@ -15,4 +10,4 @@ logger = logging.getLogger("main_bot")
 async def welcome_on_join(welcome_config: WelcomeConfig, member: discord.Member):
     channel: discord.TextChannel = member.guild.get_channel(welcome_config.welcome_channel_id)
     if channel and len(welcome_config.messages) > 0:
-        await channel.send(random.sample(welcome_config.messages, 1)[0].format(member.display_name, member.mention))
+        await channel.send(random.choice(welcome_config.messages).format(member.display_name, member.mention))
