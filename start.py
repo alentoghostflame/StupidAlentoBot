4ae48c72b0434f8430f51de4e4e484dc5750d2a5
Mrcoffeecups <mrdarkroast@gmail.com>
2020-11-20 11:34:32 -0800

    Fixed start.py
    Signed-off-by: Mrcoffeecups <mrdarkroast@gmail.com>

--- start.py
+++ start.py
@@ -1,14 +1,14 @@
 from alento_bot import StupidAlentoBot, CoreModule
 from devtool_module import DevToolModule
 from example_module import ExampleModule
-from coffee_module import CoffeeModule
+#from coffee_module import CoffeeModule
 # from moderation_module import ModeratorModule
 # from eve_module import EVEModule
 # from faq_module import FAQModule
 # from misc_module import MiscModule
-from misc2_module import MiscModule as Misc2Module
-from self_roles_module import SelfRoleModule
-from eval_module import EvalModule
+# from misc2_module import MiscModule as Misc2Module
+# from self_roles_module import SelfRoleModule
+# from eval_module import EvalModule
 # from mmo_module import MMOModule
 # from mudaemod_module import MudaeModModule
 
@@ -24,15 +24,14 @@ discord_bot = StupidAlentoBot()
 try:
     discord_bot.add_module(CoreModule)
     discord_bot.add_module(DevToolModule)
-    # discord_bot.add_module(ExampleModule)
-    discord_bot.add_module(CoffeeModule)
+    discord_bot.add_module(ExampleModule)
+    # discord_bot.add_module(CoffeeModule)
     # discord_bot.add_module(ModeratorModule)
     # discord_bot.add_module(EVEModule)
     # discord_bot.add_module(FAQModule)
     # discord_bot.add_module(MiscModule)
-    discord_bot.add_module(Misc2Module)
-    discord_bot.add_module(SelfRoleModule)
-    discord_bot.add_module(EvalModule)
+    # discord_bot.add_module(Misc2Module)
+    # discord_bot.add_module(EvalModule)
     # discord_bot.add_module(MMOModule)
     # discord_bot.add_module(MudaeModModule)
 
