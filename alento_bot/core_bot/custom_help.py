bd8734c765981b59cb93ba93d3cdaf7cee59b412
Alex Schoenhofen <alexanderschoenhofen@gmail.com>
2020-11-30 21:06:58 -0600

    Added usage line, removed commentted code block
    Signed-off-by: Alex Schoenhofen <alexanderschoenhofen@gmail.com>

--- alento_bot/core_bot/custom_help.py
+++ alento_bot/core_bot/custom_help.py
@@ -101,30 +101,6 @@ class AlentoHelpCommand(HelpCommand):
             help_embed.add_field(name="Aliases", value=f"`{'|'.join(group.aliases)}`")
         filtered = await self.filter_commands(group.commands, sort=True)
         self.add_indented_commands(help_embed, filtered, "Subcommands")
+        help_embed.add_field(name="Usage", value=f"`{self.get_command_signature(group)}`", inline=False)
 
         await destination.send(embed=help_embed)
-
-    # def add_indented_commands(self, cmds, *, heading, max_size=None):
-    #     if not cmds:
-    #         return
-    #
-    # async def send_bot_help(self, mapping):
-    #     context: commands.Context = self.context
-    #     bot: commands.Bot = context.bot
-    #     help_embed = discord.Embed(title="Bot Help", color=randint(0, 0xffffff))
-    #
-    #     if bot.description:
-    #         help_embed.add_field(name="Description", value=bot.description)
-    #
-    #     no_category = '\u200b{0.no_category}:'.format(self)
-    #
-    #     def get_category(command, *, no_category=no_category):
-    #         cog = command.cog
-    #         return cog.qualified_name + ':' if cog is not None else no_category
-    #
-    #     filtered = await self.filter_commands(bot.commands, sort=True, key=get_category)
-    #     max_size = self.get_max_size(filtered)
-    #     to_iterate = itertools.groupby(filtered, key=get_category)
-    #
-    #     for category, bot_cmds in to_iterate:
-    #         command_list = sorted(bot_cmds, key=lambda c: c.name)
