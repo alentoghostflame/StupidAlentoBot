d6627c280f797356c2fd9414dfa428e1c84e035f
Alex Schoenhofen <alexanderschoenhofen@gmail.com>
2020-12-05 22:43:37 -0600

    Added additional check to universal error handler
    Signed-off-by: Alex Schoenhofen <alexanderschoenhofen@gmail.com>

--- alento_bot/external_objects/error_handler.py
+++ alento_bot/external_objects/error_handler.py
@@ -1,5 +1,5 @@
-from discord.ext import commands
 from alento_bot.external_objects import universal_text
+from discord.ext import commands
 import logging
 
 
@@ -13,6 +13,8 @@ async def error_handler(context: commands.Context, exception: Exception, raise_e
     elif isinstance(exception, commands.MissingRequiredArgument):
         await context.send_help(context.command)
         return True
+    elif isinstance(exception, commands.NotOwner):
+        await context.send("You are not the owner.")
     elif raise_exception:
         await context.send(f"Critical error occurred, {type(exception)} {exception}")
         logger.error(f"Critical error occurred, {type(exception)} {exception}")
