4168c9f3c1a031516c5455064c4194a68f56a4a8
Alex Schoenhofen <alexanderschoenhofen@gmail.com>
2020-12-10 20:55:48 -0600

    changed wording from critical to unhandled
    Signed-off-by: Alex Schoenhofen <alexanderschoenhofen@gmail.com>

--- alento_bot/external_objects/error_handler.py
+++ alento_bot/external_objects/error_handler.py
@@ -16,8 +16,8 @@ async def error_handler(context: commands.Context, exception: Exception, raise_e
     elif isinstance(exception, commands.NotOwner):
         await context.send("You are not the owner.")
     elif raise_exception:
-        await context.send(f"Critical error occurred, {type(exception)} {exception}")
-        logger.error(f"Critical error occurred, {type(exception)} {exception}")
+        await context.send(f"Unhandled error occurred, {type(exception)} {exception}")
+        logger.error(f"Unhandled error occurred, {type(exception)} {exception}")
         raise exception
     else:
         return False
