c2bcc22711d201cf3cf67f55066a1841d40ff103
Alex Schoenhofen <alexanderschoenhofen@gmail.com>
2021-04-05 19:10:34 -0500

    Added missing return True's
    Signed-off-by: Alex Schoenhofen <alexanderschoenhofen@gmail.com>

--- alento_bot/external_objects/error_handler.py
+++ alento_bot/external_objects/error_handler.py
@@ -9,10 +9,8 @@ logger = logging.getLogger("main_bot")
 async def error_handler(context: commands.Context, exception: Exception, raise_exception: bool = True) -> bool:
     if isinstance(exception, commands.MissingPermissions):
         await context.send(universal_text.ERROR_MISSING_PERMISSION)
-        return True
     elif isinstance(exception, commands.MissingRequiredArgument):
         await context.send_help(context.command)
-        return True
     elif isinstance(exception, commands.NotOwner):
         await context.send("You are not the owner.")
     elif isinstance(exception, commands.RoleNotFound):
@@ -25,4 +23,5 @@ async def error_handler(context: commands.Context, exception: Exception, raise_e
         raise exception
     else:
         return False
+    return True
 
